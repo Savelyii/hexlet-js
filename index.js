270fda5a358fc5adfa5868abf01998a3c4e39049
savelyii <leshaboichenko95@mail.ru>
2023-01-11 22:38:41 +0300

    prettier change quotation marks

--- index.js
+++ index.js
@@ -2,13 +2,3 @@ import _ from "lodash";
 
 console.log("Hello, Hexlet!");
 console.log(_.last(["one", "two"]));
-
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
-};
-
-const fn = (a, b) => {
-  const c = a + b;
-  return c;
-};
